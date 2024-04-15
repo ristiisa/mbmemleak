197ba1e68bfd7721f47e73a75044c1936b95d244
Silver Aabjõe <ristiisa@gmail.com>
2024-04-15 10:27:55 +0300

    updated example

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
+
+List<PolylineAnnotationOptions> polylines = [];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   MapboxOptions.setAccessToken("<TOKEN HERE>");
+
+  var data = json.decode(await rootBundle.loadString("path.json")) as List;
+  for(var n = 0; n < 10; n++) polylines.addAll(data.map((e) => PolylineAnnotationOptions(
+    geometry: e,
+    lineWidth: 5.0,
+    lineColor: Colors.red.value,
+    lineJoin: LineJoin.ROUND,
+  )).toList());
 
   runApp(MaterialApp(home: MapSample()));
 }
@@ -22,38 +36,34 @@ class _MapSampleState extends State<MapSample> {
     puckBearing: PuckBearing.HEADING
   );
 
-  var lastLocation = Position(24, 59);
-
   Stream<Position>? location;
 
+  PolylineAnnotationManager? polylineAnnotationManager;
+  
   @override
   initState() {
     super.initState();
-
-    location = Stream.periodic(const Duration(seconds: 1), (_) {
-      lastLocation = Position((lastLocation.lng + 1 % 180), (lastLocation.lat + 1) % 90);
-
-      return lastLocation;
-    });
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: IndexedStack(children: [
-      Container(),
-      StreamBuilder<Position>(stream: location, builder: (context, snapshot) {
-        return MapWidget(
-          key: ValueKey(lastLocation.toJson().toString()),
-          cameraOptions: CameraOptions(center: Point(coordinates: snapshot.data ?? lastLocation), zoom: 4, pitch: 0.0),
-          onMapCreated: (MapboxMap mapboxMap) async {
+    body: MapWidget(
+      key: ValueKey("map"),
+      cameraOptions: CameraOptions(center: Point(coordinates: Position(-122.04160728, 37.33676622)), zoom: 12, pitch: 0.0),
+      onMapCreated: (mapboxMap) async {
             await Future.wait([
               mapboxMap.scaleBar.updateSettings(ScaleBarSettings(enabled: false)),
               mapboxMap.gestures.updateSettings(GesturesSettings(rotateEnabled: false)),
               mapboxMap.attribution.updateSettings(AttributionSettings(clickable: false, iconColor: 1)),
               mapboxMap.location.updateSettings(locationSettings),
             ]);
-          }
-        );
-      })
-    ]));
+        polylineAnnotationManager ??= await mapboxMap.annotations.createPolylineAnnotationManager();
+
+        Timer.periodic(const Duration(milliseconds: 100), (_) async {
+          polylineAnnotationManager!.deleteAll();
+          await polylineAnnotationManager!.createMulti(polylines!);
+        });
+      }
+    )
+  );
 }
